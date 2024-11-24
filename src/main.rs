4c1c31d256445abedcd56af25988f60fbf75c8a8
qwerty <qwerty@nexstn.com>
2024-11-24 14:40:07 +0100

    Change Main discord implementation

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
 mod huffman;
 mod service;
 mod memory;
-mod discord;
-
 mod data;
-use std::env;
 use crate::huffman::decoder::OptimizedHuffmanDecoder;
 use crate::service::reader_service::parse_stock_bytes;
+use crate::service::channel::{ DiscordChannels};
 use crate::memory::server::SharedMemoryServer;
 
 use std::time::{Instant};
 use serenity::prelude::*;
 use dotenv::dotenv;
-use serenity::model::id::ChannelId;
-use crate::data::cache::{CacheManager, OutfitSearcher};
+use crate::data::cache::{CacheManager, H9123};
 
-const CHANNEL: u64 = 1309587781252546710;
-const CHANNEL_ID: ChannelId = ChannelId::new(CHANNEL);
+//1309907112964653117
+//1309907121663508601
+//1309907129347473478
+//1309907135949180988
+
+
 
 const MAX_SHORT_BITS: usize = 8;
 const SHARED_MEMORY_SIZE: usize = 1024 * 1024; // 1MB
@@ -29,7 +30,7 @@ async fn main() {
     let cache_manager = CacheManager::new(3600);
 
     // Load cached data
-    let allCostumes = match cache_manager.load_cache("src/data/db/all.bin") {
+    let listone = match cache_manager.load_cache("src/data/db/all.bin") {
         Ok(list) => list,
         Err(err) => {
             eprintln!("Failed to load cache: {}", err);
@@ -37,7 +38,7 @@ async fn main() {
         }
     };
 
-    let allBloodys = match cache_manager.load_cache("src/data/db/bloody.bin") {
+    let listwo = match cache_manager.load_cache("src/data/db/bloody.bin") {
         Ok(list) => list,
         Err(err) => {
             eprintln!("Failed to load cache: {}", err);
@@ -46,13 +47,16 @@ async fn main() {
     };
 
     // Create searcher
-    let searcher = OutfitSearcher::new(allCostumes, allBloodys);
+    let searcher = H9123::new(listone, listwo);
+
 
     let secret ="";
     let client = Client::builder(secret, GatewayIntents::empty())
         .await
         .expect("Failed to create client");
-    let http = client.http.clone();
+
+    let discord_channels = DiscordChannels::new(client.http.clone());
+
 
     let mut server = SharedMemoryServer::new("h278", SHARED_MEMORY_SIZE)
         .expect("Failed to create shared memory server");
@@ -63,7 +67,7 @@ async fn main() {
     while server.wait_for_data() {
         let start = Instant::now();
         match server.process_data() {
-            Ok(shared_mem_data) => {
+            Ok((client_id, shared_mem_data)) => {
                 let duration = start.elapsed();
                 let decoded = decoder.decode_to_bytes(&shared_mem_data);
                 let result = parse_stock_bytes(&decoded);
@@ -75,28 +79,28 @@ async fn main() {
                         .collect();
 
                     let search_results = searcher.search_standard(&search_ids);
+                    println!("Time taken: {} ns", duration.as_nanos());
 
                     // Filter for items only in first list (has first value but no second value)
                     for (id, first_value, second_value) in &search_results {
                         if second_value.is_none() && first_value.is_some() {
-                            let message = format!(
-                                "New stock data! Item: {} (ID: {})",
-                                first_value.as_ref().unwrap(),
-                                id
-                            );
+                            if let Some(&(_, stock)) = result.iter().find(|(result_id, _)| result_id.to_string() == *id) {
+                                let message: String = format!(
+                                    "@everyone Client {}: Item: {} (ID: {}) - Stock: {}",
+                                    client_id,
+                                    first_value.as_ref().unwrap(),
+                                    id,
+                                    stock
+                                );
 
-                            // Clone http for each iteration
-                            let http = http.clone();
+                                // Clone http for each iteration
+                                discord_channels.send_message(client_id, message);
+                                println!("COMPLETE OUTFIT NOTIF TIME TAKEN: {} ns", duration.as_nanos());
 
-                            tokio::spawn(async move {
-                                if let Err(why) = CHANNEL_ID.say(&http, message).await {
-                                    eprintln!("Error sending Discord message: {:?}", why);
-                                }
-                            });
+                            }
                         }
                     }
                 }
-
             },
             Err(e) => eprintln!("Error processing data: {}", e),
         }
